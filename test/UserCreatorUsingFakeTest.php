751dd043e4d158a1383978beec9c3a6f4584bac7
isabel garrido <isa.madrid90@gmail.com>
2019-03-31 11:59:08 +0200

    Add example for invalid case using fake

--- test/UserCreatorUsingFakeTest.php
+++ test/UserCreatorUsingFakeTest.php
@@ -6,10 +6,28 @@ use DoublesMeetup\User;
 use DoublesMeetup\UserCreator;
 use PHPUnit\Framework\TestCase;
 use Test\Fake\UserRepositoryInMemory;
+use Test\Stub\UsernameValidatorInvalidStub;
 use Test\Stub\UsernameValidatorValidStub;
 
 class UserCreatorUsingFakeTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function shouldThrowExceptionWhenUsernameInvalidUsingFake()
+    {
+        $this->expectException(\Exception::class);
+
+        $userRepository = new UserRepositoryInMemory();
+        $userValidator = new UsernameValidatorInvalidStub();
+        $userCreator = new UserCreator($userValidator, $userRepository);
+
+        $userCreator->create('username', 'password');
+
+        $createdUser = $userRepository->findByUsername('username');
+        $this->assertEquals(null, $createdUser);
+    }
+
     /** @test */
     public function shouldCreateNewUserWhenUsernameValidFake()
     {
